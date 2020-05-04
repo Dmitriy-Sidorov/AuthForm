78de6d6be129644e1ec78e1ddaf1bf112c34a88f
Dmitriy Sidorov <dmitrii.sidorov@accenture.com>
2020-05-04 17:51:42 +0300

    singUpUser, loadScene

--- src/sample/database/DatabaseHandler.java
+++ src/sample/database/DatabaseHandler.java
@@ -1,7 +1,15 @@
 package sample.database;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseHandler extends Configs {
@@ -15,5 +23,45 @@ public class DatabaseHandler extends Configs {
         dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPass);
 
         return dbConnection;
+    }
+
+    public void singUpUser(String firstName, String lastName, String userName, String password, String location, String gender) {
+        String insert = "INSERT INTO " + Const.USER_TABLE + "(" + Const.USERS_FIRSTNAME + "," + Const.USERS_LASTNAME + ","
+                + Const.USERS_USERNAME + "," + Const.USERS_PASSWORD + "," + Const.USERS_LOCATION + "," + Const.USERS_GENDER + ")"
+                + "VALUES(?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement prSt = getDbConnection().prepareStatement(insert);
+            prSt.setString(1, firstName);
+            prSt.setString(2, lastName);
+            prSt.setString(3, userName);
+            prSt.setString(4, password);
+            prSt.setString(5, location);
+            prSt.setString(6, gender);
+
+            prSt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadScene (Button buttonAction, String pathScene) {
+        buttonAction.getScene().getWindow().hide();
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(pathScene));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent scene = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 }
